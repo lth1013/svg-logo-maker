c92508c598b1eb11efe2d7b968b0149a6e0453eb
unknown <lukeharriman1013@gmail.com>
2023-07-28 14:36:14 -0700

    js function bug fixing

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# svg-logo-maker
+# SVG Logo Maker
+
+## Technology Used 
+
+| Technology Used         | Resource URL           | 
+| ------------- |:-------------:|   
+
+| Git | [https://git-scm.com/](https://git-scm.com/)     |  
+|JavaScript | [https://developer.mozilla.org/en-US/docs/Web/JavaScript](https://developer.mozilla.org/en-US/docs/Web/JavaScript) |
+
+
+
+
+
+## Description 
+
+[Visit the Deployed Site](https://lth1013.github.io/svg-logo-maker/)
+
+
+
+
+
+## Usage 
+
+
+
+
+![Screenshot]()
+
+
+
+
+
+## Author Info
+
+
+### Luke Harriman
+
+* [Github](https://github.com/lth1013)
--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const shapes = require("./lib/shapes.js");
+const path = require("path");
+const shapes = {
+  circle: function (color) {
+    //depending on the color keyword passed in, the color will change, but the shape will remain the same
+    return `<svg height="100" width="100">
+        <circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" fill="${color}" />
+        </svg>`;
+  },
+  square: function (color) {
+    // creates a square with the color passed in
+    return `<svg height="100" width="100">
+        <rect width="100" height="100" style="fill:${color};stroke-width:3;stroke:rgb(0,0,0)" />
+        </svg>`;
+  },
+  triangle: function (color) {
+    // triangle with the color passed in
+    return `<svg height="100" width="100">
+        <polygon points="50,0 100,100 0,100" style="fill:${color};stroke:black;stroke-width:3" />
+        </svg>`;
+  },
+};
 
 const text = {
   text: function (text, color) {
@@ -38,20 +58,40 @@ inquirer
   })
   .catch((error) => {
     if (error) {
+      console.error(error);
     } else {
+      console.error("Something went wrong.");
     }
   });
 
 const generateSVG = (answers) => {
-  return `${shapes[answers.shape](answers["shape-color"])}
-    ${text.text(answers.text, answers["text-color"])}`;
+  // Generate the SVG shape
+  const shapeSvg = shapes[answers.shape](answers["shape-color"]);
+
+  // Generate the SVG text and position it in the center
+  const textSvg = text.text(answers.text, answers["text-color"]);
+  const textX = 50 - (answers.text.length * 2.5); // Adjusting position for centering
+  const textY = 60; // Adjusting position for centering
+
+  // Combining the shape and text SVG elements
+  return `<svg height="100" width="100">
+      ${shapeSvg}
+      <g transform="translate(${textX}, ${textY})">${textSvg}</g>
+      </svg>`;
 };
 
 const writeToFile = (fileName, data) => {
-  fs.writeFile(fileName, data, (err) => {
+  const examplesFolder = "examples"; 
+  const filePath = path.join(examplesFolder, fileName);
+
+  fs.writeFile(filePath, data, (err) => {
     if (err) {
       console.log(err);
+    } else {
+      console.log(
+        `Generated logo.svg.`
+      );
     }
-    console.log("Success!");
   });
 };
+module.exports = { shapes };
